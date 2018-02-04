f516b463e51137200b2aa37b99c5564a97f687f7
btahir <bilal@Bilals-iMac.local>
2018-02-03 21:26:41 -0800

    installed next.js
    installed next.js

---
+++ .next/dist/pages/newcampaign.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/bilal/Downloads/Smart_Contracts/Kickstarter_DApp/pages/newcampaign.js?entry';
+
+exports.default = function () {
+	return _react2.default.createElement('h1', {
+		__source: {
+			fileName: _jsxFileName,
+			lineNumber: 4
+		}
+	}, 'This is the new campaign page!');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL25ld2NhbXBhaWduLmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQU0sQUFDcEI7d0JBQU8sY0FBQTs7YUFBQTtlQUFBO0FBQUE7QUFBQSxFQUFBLEVBQVAsQUFBTyxBQUNQO0FBRkQiLCJmaWxlIjoibmV3Y2FtcGFpZ24uanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2JpbGFsL0Rvd25sb2Fkcy9TbWFydF9Db250cmFjdHMvS2lja3N0YXJ0ZXJfREFwcCJ9
